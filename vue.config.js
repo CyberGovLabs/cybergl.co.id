0942fe5ef422b65c1a60179cf56893acad16fa3a
Rizky Syaiful <rizky.syaiful@gmail.com>
2019-09-17 17:05:28 +0700

    emptying repo

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    publicPath: "/cybergl.co.id/",
-    pluginOptions: {
-        ghPages: {
-            repo: 'https://github.com/rizkysyaiful/cybergl.co.id.git'
-        },
-    },
-};

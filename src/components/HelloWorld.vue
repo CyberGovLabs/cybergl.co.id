5b7af904b72244825cfdda9d241df1a2805ad05a
Rizky Syaiful <rizky.syaiful@gmail.com>
2019-09-17 16:44:37 +0700

    correcting git remote address

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+//src/components/HelloWorld.vue
+<template>
+  <div>
+    <Cards />
+  </div>
+</template>
+
+<script>
+import Cards from './Cards.vue'
+export default {
+  name:'cards',
+  components: {
+    Cards
+  },
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+<style scoped>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    pluginOptions: {
+        ghPages: {
+            repo: 'https://github.com/CyberGovLabs/CyberGovLabs.github.io.git'
+        },
+    },
+};

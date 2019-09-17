ed60a0014382b7eb3c718ae2ef00b7a7eda9bf91
Rizky Syaiful <rizky.syaiful@gmail.com>
2019-09-17 09:43:26 +0700

    the inital vue source code

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    publicPath: "/cybergl.co.id/",
+    pluginOptions: {
+        ghPages: {
+            repo: 'https://github.com/rizkysyaiful/cybergl.co.id.git'
+        },
+    },
+};

f6de47e44c78d372368fc77b53878b293b2c49e1
Rizky Syaiful <rizky.syaiful@gmail.com>
2019-09-18 19:08:09 +0700

    install style-resources-loader

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
 module.exports = {
     pluginOptions: {
-        ghPages: {
-            repo: 'https://github.com/CyberGovLabs/CyberGovLabs.github.io.git',
-            branch: 'master'
-        },
+      ghPages: {
+        repo: 'https://github.com/CyberGovLabs/CyberGovLabs.github.io.git',
+        branch: 'master'
+      },
+      'style-resources-loader': {
+        preProcessor: 'scss',
+        patterns: []
+      }
     },
 };
